b43e7af02e0cfa5b3f6566ec42a93a9aac0dcebe
farrukh90 <farrukhsadykov@gmail.com>
2020-05-09 02:41:05 +0000

    removed region names

--- output.tf
+++ output.tf
@@ -4,22 +4,22 @@ output "vpc" {
 
 output "public_subnets" {
   value = [
-    "${aws_subnet.public1.id}         ${aws_subnet.public1.cidr_block}        ${aws_subnet.public1.availability_zone}",
-    "${aws_subnet.public2.id}         ${aws_subnet.public2.cidr_block}        ${aws_subnet.public2.availability_zone}",
-    "${aws_subnet.public3.id}         ${aws_subnet.public3.cidr_block}        ${aws_subnet.public3.availability_zone}",
+    "${aws_subnet.public1.id}",
+    "${aws_subnet.public2.id}",
+    "${aws_subnet.public3.id}",
   ]
 }
 
 output "private_subnets" {
   value = [
-    "${aws_subnet.private1.id}         ${aws_subnet.private1.cidr_block}        ${aws_subnet.private1.availability_zone}",
-    "${aws_subnet.private2.id}         ${aws_subnet.private2.cidr_block}        ${aws_subnet.private2.availability_zone}",
-    "${aws_subnet.private3.id}         ${aws_subnet.private3.cidr_block}        ${aws_subnet.private3.availability_zone}",
+    "${aws_subnet.private1.id}",
+    "${aws_subnet.private2.id}",
+    "${aws_subnet.private3.id}",
   ]
 }
 
 output "region" {
-  value = "${var.region}         ${var.region_name}"
+  value = "${var.region}"
 }
 
 output "Tags" {
