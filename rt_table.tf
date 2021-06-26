ae46e8b6b6596aa15bb1da34615469b5c4c6af82
Farrukh Sadykov <farrukhsadykov@gmail.com>
2021-06-25 21:26:45 -0500

    Update rt_table.tf

--- rt_table.tf
+++ rt_table.tf
@@ -27,16 +27,3 @@ resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
   tags   = var.tags
 }
-
-resource "aws_eip" "nat" {
-  vpc  = true
-  tags = var.tags
-}
-
-resource "aws_nat_gateway" "gw" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public1.id
-  tags          = var.tags
-}
-
-
